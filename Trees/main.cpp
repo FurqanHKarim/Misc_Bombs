ceae2aef147b83a6d2d472aac93748e1d4bad899
FurqanHKarim <furqanhameedkarim63@gmail.com>
2023-08-09 18:58:50 +0500

    Worked on Trees

--- Trees/main.cpp
+++ Trees/main.cpp
@@ -5,104 +5,149 @@
 using namespace std;
 
 
+enum class Color{Red,Black};
 
 struct Node {
-	int Value;
+	double Value;
 	Node* R_Next;
 	Node* L_Next;
+    Color current_color;
+    int depth;
 
 
 
 	Node() {
-		R_Next = NULL;
-		L_Next = NULL;
+		R_Next = nullptr;
+		L_Next = nullptr;
+        depth = 0;
+        current_color = Color::Red;
 	}
 
-	Node(int _Value) {
+	Node(double _Value) {
 		Value = _Value;
-		R_Next = NULL;
-		L_Next = NULL;
+		R_Next = nullptr;
+		L_Next = nullptr;
+        depth = 0;
+        current_color = Color::Red;
 	}
     
 };
-
 class BinaryTree {
 private:
+    int max_depth;
  	Node* DaNode;
+
     static void preOrderPrint(Node* boi);
 
 
 public:
+    double getshortest(Node* &sample);
     BinaryTree();
-    BinaryTree(int _Value);
-    void insersion(int _Value); 
+    BinaryTree(double _Value);
+    void insersion(double _Value); 
     void preOrderVisit();
+    Node* search(int sample);
+    Node* searchparent(int sample);
+    Node* leftRotate(Node* &temp);
+    void remove(int value);
 };
 
 
-bool palindromechecker(string input){
-    
-    string::iterator t1;
-    string::reverse_iterator t2;
-    t1 = input.begin();
-    t2 = input.rbegin();
-    for (int i = 0; i < input.length(); i++)
-    {
-        if(*t1 != *t2){
-            return 0;
-        }
-        t1++;
-        t2++;
-    }
-    return 1;
-}
-
 
 int main() {
-    cout<<palindromechecker("racecar");
+    BinaryTree hello(7);
+    hello.insersion(5);
+    hello.insersion(12);
+    hello.insersion(11);
+    hello.insersion(19);
+    hello.insersion(13);
+    hello.insersion(12.5);
+    hello.insersion(12.7);
+    hello.insersion(14);
+    hello.insersion(25);
+    hello.insersion(6);
+    hello.insersion(4);
+    hello.insersion(2);
+    hello.remove(2);
+  //  Node* wut = hello.getshortest(hello.search(12));
 
 
+    hello.preOrderVisit();
+    
+    hello.search(88);
 
-    queue<int> what;
-    what.push(9);
-    what.push(8);
-    what.push(7);
-    what.push(6);
-    what.push(5);
-    what.push(4);
-    what.push(3);
-    what.push(2);
-    what.pop();
-    int a = what.back();
-    a = what.emplace();
-    a = what.empty();
-    a = what.front();
-    what.pop();
-    a = what.size();
-    what.swap(what);
+    cout<<"Done :  "<<endl;
 
-    stack<char> why;
-    why.push('w');
-    why.push('h');
-    why.push('y');
-    why.push(' ');
-    why.push('i');
-    why.push('s');
-    why.push(' ');
-    why.push('y');
-    why.push('o');
-    why.push('u');
-    why.push('?');
-
-    why.pop();
-    cout<<why.empty();
-    while(!why.empty())
-    {
-        cout<<why.top();
-        why.pop();
-    }
 
     
+
+}
+
+double BinaryTree:: getshortest(Node* &sample)
+{   
+    double temp1;
+
+    if(sample->L_Next == nullptr){
+        temp1 = sample->Value;
+        delete sample;
+        return temp1;
+    }
+    Node* temp = nullptr;
+
+    while(sample->L_Next->L_Next != nullptr)
+    {
+        sample = sample->L_Next;
+    }
+
+    temp = sample->L_Next;
+    sample->L_Next = temp->R_Next;
+    temp->R_Next = nullptr;
+    temp1 = temp->Value;
+    delete temp;
+    return temp1;
+
+
+}
+
+void BinaryTree:: remove(int value)
+{
+    Node* temp = this->DaNode;
+    temp = search(value);
+    if (temp == nullptr)
+    {
+        return;
+    }
+
+    if(temp->R_Next == nullptr){
+        Node* temp1 = temp->L_Next;
+        if(temp1 == nullptr)
+        {   
+            temp->L_Next = nullptr;
+            delete temp;
+            return;
+        }
+        if(temp->L_Next->Value == value){
+            temp1 = temp->L_Next;
+            temp->L_Next = nullptr;
+            delete temp1;
+            return;
+            //this is fix check this and work on it
+        }
+
+        temp->L_Next = temp1->L_Next;
+        temp->R_Next = temp1->R_Next;
+        temp->current_color = temp1->current_color;
+        temp->depth = temp1->depth;
+        temp->Value = temp1->Value;
+        delete temp1;
+        return;
+    }
+    temp->Value = getshortest(temp->R_Next);
+}
+
+
+Node* leftRotate(Node* &temp){
+    Node* inter = temp;
 
 }
 
@@ -111,44 +156,101 @@ int main() {
 
 
 
+ 
+Node* BinaryTree:: searchparent(int sample){
+    Node* inter;
+    Node* parent_iter;
+    inter = DaNode;
+    while(1){
+        parent_iter = inter;
+        if(inter==nullptr){
+            cout << "sample not found" << endl;
+            return nullptr;
+        }
+
+        if(sample>inter->Value){
+            inter = inter->R_Next;
+            continue;
+        }else if(sample<inter->Value)
+        {
+            inter = inter->L_Next;
+            continue;
+        }
+
+        if(inter->Value == sample){
+            return parent_iter;
+        }
+    }
+}
 
 
+Node* BinaryTree:: search(int sample){
+    Node* inter;
+    Node* edge_case;
+    inter = DaNode;
+    while(1){
+        if(inter==nullptr){
+            cout << "sample not found" << endl;
+            return nullptr;
+        }
 
+        if(inter->Value == sample){
+            //cout<<"Sample Found:: at depth"<<inter->depth;
+            if((inter->L_Next==nullptr)&&(inter->R_Next==nullptr))
+                return edge_case;
 
+            return inter;
+        }
+        edge_case=inter;
 
+        if(sample>inter->Value){
+            inter = inter->R_Next;
+            continue;
+        }else if(sample<inter->Value)
+        {
+            inter = inter->L_Next;
+            continue;
+        }
 
+    }
+}
 
 
 BinaryTree:: BinaryTree() {
-
-	}
-BinaryTree:: BinaryTree(int _Value) {
-		DaNode = new Node(_Value);
-	}
-void BinaryTree:: insersion(int _Value) 
+    DaNode = nullptr;
+    max_depth = 0;
+}
+BinaryTree:: BinaryTree(double _Value) {
+    max_depth = 0;
+    DaNode = new Node(_Value);
+}
+void BinaryTree:: insersion(double _Value) 
 {
-
-
     //there are only a few cases where you can insert a new node
     //just make this new Node (first order of business)
     Node* new_boi = new Node(_Value);
     Node* inter;
     inter = this->DaNode;
     // 1) if there is no root
-    if (inter == NULL)
+    if (inter == nullptr)
     {
+        new_boi->current_color = Color::Black;
         DaNode = new_boi;
+
         return;
     }
     while (1)
     {
-        
+        new_boi->depth++;
+        if(new_boi->depth>max_depth)
+            max_depth = new_boi->depth; 
+
 
         if (_Value < inter->Value)
         {
             // 2) if there isnt a Node on the Left
-            if (inter->L_Next == NULL)
-            {
+            if (inter->L_Next == nullptr)
+            {   
                 inter->L_Next = new_boi;
                 return;
             }
@@ -163,7 +265,7 @@ void BinaryTree:: insersion(int _Value)
         if (_Value > inter->Value || _Value == inter->Value)
         {
             // 4) if there is no Node on the Right
-            if (inter->R_Next == NULL)
+            if (inter->R_Next == nullptr)
             {
                 inter->R_Next = new_boi;
                 return;
@@ -182,31 +284,31 @@ void BinaryTree::  preOrderVisit(){
 }
 void BinaryTree:: preOrderPrint(Node* boi){
 
-    if(boi == NULL)
+    if(boi == nullptr)
     {
         return;
     }
     
-    cout<<boi->Value<<endl;
     preOrderPrint(boi->L_Next);
+    cout<<boi->Value<<endl;
     preOrderPrint(boi->R_Next);
 
 
-    // if(boi->L_Next != NULL)
+    // if(boi->L_Next != nullptr)
     // {
     //     preOrderPrint(boi->L_Next);
     // }
-    // if (boi->R_Next != NULL)
+    // if (boi->R_Next != nullptr)
     // {
     //     preOrderPrint(boi->R_Next);
     // }
 
     // cout<<boi->Value<<endl;
 
-    // if(boi->L_Next == NULL)
+    // if(boi->L_Next == nullptr)
     // {
     //     cout<<boi->Value<<endl;
-    //     if (boi->R_Next == NULL)
+    //     if (boi->R_Next == nullptr)
     //     {
     //         return;
     //     }
@@ -219,7 +321,7 @@ void BinaryTree:: preOrderPrint(Node* boi){
     // {
     //     preOrderPrint(boi->L_Next);
     //     cout<<boi->Value<<endl;
-    //     if (boi->R_Next != NULL)
+    //     if (boi->R_Next != nullptr)
     //     {
     //         preOrderPrint(boi->R_Next);
     //     }
